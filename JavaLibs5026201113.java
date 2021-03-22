e91c31b25f180f2961801fa91307d4d0344dcd21
muhammadirfan757 <81101547+muhammadirfan757@users.noreply.github.com>
2021-03-22 07:40:54 +0700

    Add files via upload

--- JavaLibs5026201113.java
+++ JavaLibs5026201113.java
@@ -1,7 +1,7 @@
 package homework2;
 import javax.swing.JOptionPane;
 
-public class Main {
+public class JavaLibs5026201113 {
 
     public static void main(String[] args) {
     	JOptionPane.showMessageDialog(null,"Welcome to this little Q&A game, I will give you several question for you to answer");
@@ -16,15 +16,15 @@ public class Main {
 		double ips = Double.parseDouble(JOptionPane.showInputDialog(null,"How much your last semester's IPS?"));
 		int birthnum = Integer.parseInt(JOptionPane.showInputDialog(null,"How about your Mom's birthday date?"));
 		int	tall = Integer.parseInt(JOptionPane.showInputDialog(null,"How many sisters/brothers do you have?"));
-		int favnumber = Integer.parseInt(JOptionPane.showInputDialog(null,"What about your favourite number?"));
-		int total = favnumber + tall;
+		double favnumber = Integer.parseInt(JOptionPane.showInputDialog(null,"What about your favourite number?"));
+		double total = favnumber + tall;
 		String input5 = JOptionPane.showInputDialog(null,"What's your best birthday gift?");
 		String input6 =	JOptionPane.showInputDialog(null,"And how do you feel?");
 
 		JOptionPane.showMessageDialog(null,"Before you end it, I heard that you just went from a holiday with your love."
 									+"\nIt's a text from your love, you have to see it!");
 
-		JOptionPane.showMessageDialog(null,"So I went for a holiday with " + input2 + " and we went for a dinner on " + input3
+		JOptionPane.showMessageDialog(null,"So I went for a holiday with " + input2 + " and I felt so " + input1 + " .Then, we went for a dinner on " + input3
 									+ "\nat " + input4 + ". It's pretty good restaurant and for me It's "
 									+ ips + " of 5.0 star " + ".\nBtw, It's our anniversary day which consist on " + birthnum + " of August"
 									+ ".\nAnd then we went on beach around " + total + " km away from the Mall"
